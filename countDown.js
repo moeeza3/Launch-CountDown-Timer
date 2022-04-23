b388e78587060dc729c7bfe4cece64d4134e3bf2
moeea3 <moeeza3@gmail.com>
2022-04-23 22:01:00 +0500

    First commit

---
+++ countDown.js
@@ -0,0 +1,22 @@
+let countTime=setInterval(function(){
+    let enddate=new Date("Apr 30, 2022 00:00:00").getTime();
+    var now=new Date().getTime();
+    var real=enddate-now;
+    
+    
+    var days=Math.floor(real/(1000*60*60*24));
+    var hours=Math.floor((real%(1000*60*60*24))/(1000*60*60));
+    var minutes=Math.floor((real%(1000*60*60))/(1000*60));
+    var seconds=Math.floor((real%(1000*60))/(1000)); 
+    
+    days=days<10? "0"+days:days;
+    hours=hours<10? "0"+hours:hours;
+    minutes=minutes<10? "0"+minutes:minutes;
+    seconds=seconds<10? "0"+seconds:seconds;
+
+    document.getElementById("days").innerHTML=days;
+    document.getElementById("hours").innerHTML=hours;
+    document.getElementById("minutes").innerHTML=minutes;
+    document.getElementById("seconds").innerHTML=seconds;
+  },1000)
+  
